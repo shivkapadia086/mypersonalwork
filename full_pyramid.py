75d8821127730b74a8cefe277533ed2586eb772b
shivkapadia086 <shivkapdia38@gmail.com>
2026-02-27 03:45:20 +0530

    practical 4 done

--- full_pyramid.py
+++ full_pyramid.py
@@ -3,3 +3,15 @@ rows = 5
 for i in range(1, rows + 1):
     print(" " * (rows - i), end="")
     print("* " * i)
+    
+rows = 5
+
+for i in range(rows, 0, -1):
+    # Print leading spaces
+    print(" " * (rows - i), end="")
+    
+    # Print stars
+    for j in range(i):
+        print("* ", end="")
+    
+    print()
--- odd_numbers.py
+++ odd_numbers.py
@@ -8,3 +8,16 @@ print("Odd Numbers:", odd)
 print("Minimum:", min(odd))
 print("Maximum:", max(odd))
 print("Sum:", sum(odd))
+
+even_numbers = []
+
+for num in range(1, 101):
+    if num % 2 == 0:
+        even_numbers.append(num)
+
+print("List of even numbers between 1 to 100:")
+print(even_numbers)
+
+print("Minimum even number:", min(even_numbers))
+print("Maximum even number:", max(even_numbers))
+print("Total sum of even numbers:", sum(even_numbers))
